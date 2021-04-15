7876583e41a950e8b34bea72c0ae9befaa664221
ravireddym6 <ravikiranreddy.maddi@gmail.com>
2021-04-15 23:13:02 +0200

    commit now

---
+++ hellowordl1.py
@@ -0,0 +1,51 @@
+{
+ "metadata": {
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5-final"
+  },
+  "orig_nbformat": 2,
+  "kernelspec": {
+   "name": "python385jvsc74a57bd00d143373db4a309031503206dfdc2558f8e7e0ddec1f4ddd580fb0b271898011",
+   "display_name": "Python 3.8.5 64-bit (conda)"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2,
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "output_type": "stream",
+     "name": "stdout",
+     "text": [
+      "hello world\n125\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"hello world\")\n",
+    "sum = lambda x, y:x + y*5\n",
+    "print(sum(5,24))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ]
+}
---
+++ helloworld.py
@@ -0,0 +1,3 @@
+print("hello world")
+pwr = lambda x: x^2
+print(pwr(10))
